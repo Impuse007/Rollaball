f6e61f54e241567cbc97ce585bda7ca918e911b9
Aidan Kinnaird <aidansdk@gmail.com>
2023-09-21 14:32:40 -0300

    Added Level Transition and Second level

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void TransitionScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
     public void Exit()
     {
         Application.Quit();
